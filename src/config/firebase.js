b050272d2b2a140a35b325811ad8d960ff5f3a0e
Yoselyn Morle <103976921+gabriellavetencourtc@users.noreply.github.com>
2024-03-08 17:31:29 -0400

    Proyecto en blanco + firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAURu9xHF4cXEkf4kVyuXWaxf4XQzSBkVk',
+  authDomain: 'mp2-grossmann-vetencourt-76c1a.firebaseapp.com',
+  projectId: 'mp2-grossmann-vetencourt',
+  storageBucket: 'mp2-grossmann-vetencourt.appspot.com',
+  messagingSenderId: '653508378792',
+  appId: '1:653508378792:web:95d107f4295883f694829d',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
